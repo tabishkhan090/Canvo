d3c5a46c72206243dd36d51a24221f01553a4598
MD TABISH <tabishrazakhan92@gmail.com>
2025-09-17 14:08:13 +0530

    middleware added

---
+++ apps/http-BE/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express"
+const app = express();
+app.post("/signup",(req,res)=>{
+
+})
+app.post("/signin",(req,res)=>{
+
+})
+app.post("/room",(req,res)=>{
+
+})
+app.listen(3003)
---
+++ apps/http-BE/src/middleware.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config"
+
+export function middleware(req: Request, res: Response, next: NextFunction){
+    const token = req.headers["authorization"] ?? "";
+    const decoded = jwt.verify(token,JWT_SECRET)
+    if(!decoded){
+        res.json({
+            message: "wrong credentials"
+        })
+        return;
+    }
+    //@ts-ignore
+    req.userId = decoded._id;
+    next();
+}
